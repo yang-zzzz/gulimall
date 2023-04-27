d1eac44c578e1168bbf6959aed82233a11ba7bfe
yang <1142108345@qq.com>
2023-04-27 10:56:22 +0800

    first

---
+++ gulimall-coupon/src/test/java/com/yang/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yang.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
